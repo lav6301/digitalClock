f06f955243a795a04fbf3e0f86694c5a83989b8b
lav6301 <lavanyakf@gmail.com>
2024-06-04 22:49:42 +0530

    feat:adding javascript code to digital clock for getting current time

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function updateClock(){
+    const now = new Date();
+    const hours = now.getHours();
+    const meridiem = hours >=12 ? "PM" : "AM";
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+// updated clock time by interval of call back function
+setInterval(updateClock, 1000);
