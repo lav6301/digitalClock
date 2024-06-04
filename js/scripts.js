cf58fb06d341aa8e46b5bbeba099edf0705cd393
lav6301 <lavanyakf@gmail.com>
2024-06-04 23:30:35 +0530

    feat:added the sound file for alarm sound

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// create a variable for alarm and sound
+let alarmTime = null;
+let alarmTimeout = null;
+
+var clockUser=document.querySelector(".clockButton1").Onclick();{
+const alarmSound = new Audio('./sound/Tic-Tac-Mechanical-Alarm-Clock-2-chosic.com_.mp3');
+alarmSound.play();
+}
+
 function updateClock(){
     const now = new Date();
     const hours = now.getHours();
@@ -8,6 +17,17 @@ function updateClock(){
     document.getElementById("clock").textContent = timeString;
 }
 
+// check alarm with clock
+
+
+
+
+
+
+
+
+
 updateClock();
 // updated clock time by interval of call back function
 setInterval(updateClock, 1000);
+
